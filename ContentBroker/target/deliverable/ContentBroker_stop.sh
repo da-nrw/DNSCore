31a3036dee19a84d2bdfc4860cb00371429b9269
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2014-01-08 17:49:14 +0100

    remove target folder

--- ContentBroker/target/deliverable/ContentBroker_stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-kill -9 `ps -aef | grep ContentBroker.jar | grep -v grep | awk '{print $2}'`
-
-rm -f /tmp/cb.running
--- ContentBroker/target/deliverable/fido.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PYTHON_PATH="PYTHON_BIN"
-FIDO_PATH="fido/fido.py"
-
-OUT=`$PYTHON_PATH $FIDO_PATH "$1" | grep "OK" | awk 'BEGIN {FS=",";};{printf $3",";}'` 
-
-length=${#OUT}
-printf ${OUT:0:(($length - 1))}
