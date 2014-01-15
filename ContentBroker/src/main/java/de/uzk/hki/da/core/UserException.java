3e48c73c04e68c43f857dee36afb1496b122670d
Thomas Kleinke <tkleink0@uni-koeln.de>
2014-01-15 12:37:51 +0100

    adjusted container format check and error messages in UnpackAction

--- ContentBroker/src/main/java/de/uzk/hki/da/core/UserException.java
+++ ContentBroker/src/main/java/de/uzk/hki/da/core/UserException.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class UserException extends RuntimeException {
 
 	public enum UserExceptionId {
+		UNSUPPORTED_CONTAINER_FORMAT,
 		SIP_PREMIS_NOT_FOUND,
 		INVALID_SIP_PREMIS,
 		INVALID_OBJECT_DELTA,
