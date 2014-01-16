4d34f5c7b569224d5d7c2e62b3c61f3cf57ee0ed
jenspeters <peters@kcg.de>
2014-01-16 15:28:36 +0100

    Changed daweb3_proerties.grrovy to user Home

--- DAWeb/grails-app/conf/DataSource.groovy
+++ DAWeb/grails-app/conf/DataSource.groovy
@@ -7,25 +7,13 @@ hibernate {
 environments {
 	test {
 		// set per-environment irods config in classpath file!
-		grails.config.locations = ["classpath:${appName}_properties.groovy"]
+		grails.config.locations = ["file:${userHome}/.grails/${appName}_properties.groovy"]	
 	}	
 	production {
 		// set per-environment irods config in classpath file!
-		grails.config.locations = ["classpath:${appName}_properties.groovy"]
+		grails.config.locations = ["file:${userHome}/.grails/${appName}_properties.groovy"]		
 	}
 	development {
-		
-		dataSource {
-			pooled = true
-			driverClassName = "org.postgresql.Driver"
-			dialect = org.hibernate.dialect.PostgreSQLDialect
-			dbCreate = "validate"
-			url = "jdbc:postgresql://da-nrw-vm3.hki.uni-koeln.de:5435/contentbroker?autoReconnect=true"
-			username = "irods"
-			password = "98Q4P4ZgUWCey8PHPYxM3g=="
-			passwordEncryptionCodec = "de.uzk.hki.da.utils.DESCodec"
-			characterEncoding = "UTF-8"
-		}
-		
+		grails.config.locations = ["file:${userHome}/.grails/${appName}_properties.groovy"]				
 	}
 }
