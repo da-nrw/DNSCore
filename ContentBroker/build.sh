1d8de476c168d416f79b12b4896358f36e1edd28
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2014-01-22 18:52:12 +0100

    Further improvements of installation procedure.

--- ContentBroker/build.sh
+++ ContentBroker/build.sh
@@ -2,7 +2,7 @@
 
 # Author: Daniel M. de Oliveira
 
-REPO=../installation/
+INSTALLER=../installation/
 VERSION=`cat ../VERSION.txt`
 
 #
@@ -11,89 +11,84 @@ VERSION=`cat ../VERSION.txt`
 # $3 debug level for logback.xml
 #
 function prepareCustomInstallation(){
-	cp src/main/conf/jhove.conf $REPO/jhove.conf
-	cp src/main/scripts/jhove $REPO/jhove
-	cp src/main/conf/beans.xml.$1 $REPO/beans.xml
+	cp src/main/conf/beans.xml.$1 $INSTALLER/beans.xml
 	if [ $2 != "none" ]
 	then
-		cp src/main/conf/hibernateCentralDB.cfg.xml.$2 $REPO/hibernateCentralDB.cfg.xml
+		cp src/main/conf/hibernateCentralDB.cfg.xml.$2 $INSTALLER/hibernateCentralDB.cfg.xml
 	fi
-	cp src/main/conf/logback.xml.$3 $REPO/logback.xml
+	cp src/main/conf/logback.xml.$3 $INSTALLER/logback.xml
 }
 
 function createStorageFolder(){
-	mkdir $TARGET/storage/
-	mkdir $TARGET/storage/grid
-	mkdir -p $TARGET/storage/dip/institution/TEST
-	mkdir -p $TARGET/storage/dip/public/TEST
-	mkdir -p $TARGET/storage/user/TEST/outgoing
-	mkdir -p $TARGET/storage/fork/TEST
-	mkdir -p $TARGET/storage/ingest/TEST
+	mkdir $CBTAR_SRC/storage/
+	mkdir $CBTAR_SRC/storage/grid
+	mkdir -p $CBTAR_SRC/storage/dip/institution/TEST
+	mkdir -p $CBTAR_SRC/storage/dip/public/TEST
+	mkdir -p $CBTAR_SRC/storage/user/TEST/outgoing
+	mkdir -p $CBTAR_SRC/storage/fork/TEST
+	mkdir -p $CBTAR_SRC/storage/ingest/TEST
 }
 
-mvn clean
-mvn package
 
-TARGET=target/deliverable
-rm -rf $TARGET
-mkdir $TARGET
-cp target/ContentBroker-$VERSION.jar $TARGET/ContentBroker.jar
+
+mvn clean
+cd ../installation
+rm beans.xml 2>/dev/null
+rm config.properties 2>/dev/null
+rm hibernateCentralDB.cfg.xml 2>/dev/null
+rm logback.xml 2>/dev/null
+rm ffmpeg.sh 2>/dev/null
+cd ../ContentBroker 
+
+
+mvn package
+CBTAR_SRC=target/deliverable
+mkdir $CBTAR_SRC
+cp target/ContentBroker-$VERSION.jar $CBTAR_SRC/ContentBroker.jar
 if [ $? -ne 0 ]
 then
 	echo target has to be build first by mvn package
 	exit
 fi
 
-mkdir $TARGET/conf
-cp -r src/main/fido $TARGET
-cp -r src/main/jhove $TARGET
-cp -r src/main/xslt $TARGET/conf
-cp src/main/resources/premis.xsd $TARGET/conf
-cp src/main/resources/xlink.xsd $TARGET/conf
-cp src/main/resources/frame.jsonld $TARGET/conf
-cp src/main/scripts/ffmpeg.sh $TARGET
-cp src/main/scripts/ContentBroker_stop.sh $TARGET
-cp src/main/scripts/ContentBroker_start.sh $TARGET
-cp src/main/scripts/cbTalk.sh $TARGET
-cp src/main/scripts/fido.sh $TARGET
-cp src/main/scripts/PDFA_def.ps $TARGET/conf
-cp src/main/resources/healthCheck.avi $TARGET/conf
-cp src/main/resources/healthCheck.tif $TARGET/conf
-cp src/main/resources/frame.jsonld $TARGET/conf
-mkdir $TARGET/log
-touch $TARGET/log/contentbroker.log
-echo -e "ContentBroker Version $VERSION\nWritten by\n Daniel M. de Oliveira\n Jens Peters\n Sebastian Cuy\n Thomas Kleinke" > $TARGET/README.txt
 
-cd $TARGET
-rm ../../../installation/ContentBroker.tar
-tar cf ../../../installation/ContentBroker.tar *
-cd ../../../installation
-rm fido.sh > /dev/null
-rm beans.xml > /dev/null
-rm config.properties.cb > /dev/null
-rm hibernateCentralDB.cfg.xml > /dev/null
-rm jhove.conf > /dev/null
-rm logback.xml > /dev/null
-rm ffmpeg.sh > /dev/null
-rm jhove > /dev/null
-rm jhove.conf   > /dev/null
-cd ../ContentBroker
+mkdir $CBTAR_SRC/conf
+cp -r src/main/fido $CBTAR_SRC
+cp -r src/main/jhove $CBTAR_SRC
+cp src/main/scripts/jhove $CBTAR_SRC/jhove
+cp src/main/conf/jhove.conf $CBTAR_SRC/jhove/conf
+cp -r src/main/xslt $CBTAR_SRC/conf
+cp src/main/resources/premis.xsd $CBTAR_SRC/conf
+cp src/main/resources/xlink.xsd $CBTAR_SRC/conf
+cp src/main/resources/frame.jsonld $CBTAR_SRC/conf
+cp src/main/scripts/ffmpeg.sh $CBTAR_SRC
+cp src/main/scripts/ContentBroker_stop.sh $CBTAR_SRC
+cp src/main/scripts/ContentBroker_start.sh $CBTAR_SRC
+cp src/main/scripts/cbTalk.sh $CBTAR_SRC
+cp src/main/scripts/fido.sh $CBTAR_SRC
+cp src/main/scripts/PDFA_def.ps $CBTAR_SRC/conf
+cp src/main/resources/healthCheck.avi $CBTAR_SRC/conf
+cp src/main/resources/healthCheck.tif $CBTAR_SRC/conf
+cp src/main/resources/frame.jsonld $CBTAR_SRC/conf
+mkdir $CBTAR_SRC/log
+touch $CBTAR_SRC/log/contentbroker.log
+echo -e "ContentBroker Version $VERSION\nWritten by\n Daniel M. de Oliveira\n Jens Peters\n Sebastian Cuy\n Thomas Kleinke" > $CBTAR_SRC/README.txt
 
 case "$1" in
 dev)
-	sed "s@CONTENTBROKER_ROOT@$2@" src/main/conf/config.properties.dev  > $REPO/config.properties.cb
+	sed "s@CONTENTBROKER_ROOT@$2@" src/main/conf/config.properties.dev  > $INSTALLER/config.properties
 	createStorageFolder	
-	cp -f src/main/scripts/ffmpeg.sh.fake $REPO/ffmpeg.sh
+	cp -f src/main/scripts/ffmpeg.sh.fake $INSTALLER/ffmpeg.sh
 	cp src/main/conf/sqltool.rc ~/
 	prepareCustomInstallation node hsql debug
 ;;
 vm3)
-	cp src/main/conf/config.properties.vm3 $REPO/config.properties.cb
+	cp src/main/conf/config.properties.vm3 $INSTALLER/config.properties
 	prepareCustomInstallation node postgres debug
-    INSTALL_PATH=/data/danrw/ContentBroker
+	INSTALL_PATH=/data/danrw/ContentBroker
 ;;
 integration)
-	cp src/main/conf/config.properties.vm3 $REPO/config.properties.cb
+	cp src/main/conf/config.properties.vm3 $INSTALLER/config.properties
 	prepareCustomInstallation integration postgres debug
 ;;
 full)
@@ -106,6 +101,14 @@ pres)
 	prepareCustomInstallation pres none debug
 ;;
 esac
+
+
+
+cd $CBTAR_SRC
+rm ../../../installation/ContentBroker.tar
+tar cf ../../../installation/ContentBroker.tar *
+cd ../ContentBroker
+
 
 
 
--- installation/install.sh
+++ installation/install.sh
@@ -11,42 +11,51 @@ SunOS)
 	TAR=gtar
 	;;
 esac
-	
 
 
-
-
-TARGET=$1
-if [[ "${TARGET:${#TARGET}-1}" == "/" ]]; then
-	TARGET="${TARGET%?}"
+INSTALLATION_TARGET=$1
+if [[ "${INSTALLATION_TARGET:${#INSTALLATION_TARGET}-1}" == "/" ]]; then
+	INSTALLATION_TARGET="${INSTALLATION_TARGET%?}"
 fi	
-if [ ! -d "$TARGET" ]; then
-	echo Error: $TARGET is not a directory.
+if [ ! -d "$INSTALLATION_TARGET" ]; then
+	echo Error: $INSTALLATION_TARGET is not a directory.
 		exit
 fi
 HOME=`pwd`
-if [ $HOME = $TARGET ]; then 
-	echo Error target environment $TARGET and current src tree are identical!
+if [ $HOME = $INSTALLATION_TARGET ]; then 
+	echo Error target environment $INSTALLATION_TARGET and current src tree are identical!
 	exit
 fi
 
 
-echo Installing to $TARGET
-$TAR xf ContentBroker.tar -C $TARGET
-if [ -e config.properties.cb ]
+echo Installing to $INSTALLATION_TARGET
+$TAR xf ContentBroker.tar -C $INSTALLATION_TARGET
+if [ -e config.properties ]
 then
-	cp config.properties.cb $TARGET/conf/config.properties
+	if [ -e $INSTALLATION_TARGET/conf/config.properties ]
+	then
+		echo Installer found an existing config.properties at the target.
+		echo Please either delete the config.properties from the installation dir or from the target, depending on which one you want to keep.
+		exit
+	fi
+	cp config.properties $INSTALLATION_TARGET/conf/
 fi
-cp hibernateCentralDB.cfg.xml $TARGET/conf/
-cp jhove $TARGET/jhove/
-cp jhove.conf $TARGET/jhove/conf/
-cp beans.xml $TARGET/conf
-cp logback.xml $TARGET/conf
-cp configure.sh $TARGET/
+if  [ -e hibernateCentralDB.cfg.xml ]
+then
+	if [ -e $INSTALLATION_TARGET/conf/hibernateCentralDB.cfg.xml ]
+	then
+		echo Installer found an existing hibernateCentralDB.cfg.xml at the target.
+		echo Please either delete the hibernateCentralDB.cfg.xml from the installation dir or from the target, depending on which one you want to keep.
+		exit
+	fi
+	cp hibernateCentralDB.cfg.xml $INSTALLATION_TARGET/conf/
+fi
+cp beans.xml $INSTALLATION_TARGET/conf
+cp logback.xml $INSTALLATION_TARGET/conf
+cp configure.sh $INSTALLATION_TARGET/
 if [ -e ffmpeg.sh ] 
 then
-	cp -f ffmpeg.sh $TARGET/
+	cp -f ffmpeg.sh $INSTALLATION_TARGET/
 fi
-
-cd $TARGET
+cd $INSTALLATION_TARGET
 ./configure.sh
