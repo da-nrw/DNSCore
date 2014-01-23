7b5a2a65aed67555e20430ca8e487cc114ab083b
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2014-01-23 03:20:05 +0100

    added cleanup

---
+++ ContentBroker/cleanUpInstaller.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# author: Daniel M. de Oliveira
+
+echo clean up installer
+cd ../installation
+rm ContentBroker.tar 2>/dev/null
+rm beans.xml 2>/dev/null
+rm config.properties 2>/dev/null
+rm hibernateCentralDB.cfg.xml 2>/dev/null
+rm logback.xml 2>/dev/null
+rm ffmpeg.sh 2>/dev/null
+rm VERSION.txt 2>/dev/null
+cd ../ContentBroker 
