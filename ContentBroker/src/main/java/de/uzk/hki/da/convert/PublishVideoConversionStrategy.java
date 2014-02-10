f015ee936b0f6097940a6d404772160cb03a7f86
Thomas Kleinke <tkleink0@uni-koeln.de>
2014-02-10 16:21:05 +0100

    fixed PublishVideoConversionStrategyTests

--- ContentBroker/src/main/java/de/uzk/hki/da/convert/PublishVideoConversionStrategy.java
+++ ContentBroker/src/main/java/de/uzk/hki/da/convert/PublishVideoConversionStrategy.java
@@ -101,7 +101,7 @@ public class PublishVideoConversionStrategy extends PublishConversionStrategyBas
 		e.setDate(new Date());
 		
 		String cmdINSTITUTION[] = new String[]{
-				"HandBrakeCLI",
+				"./handBrake.sh",
 				"-i",
 				"\"" + ci.getSource_file().toRegularFile().getAbsolutePath() + "\"",
 				"-o",
