ff9edf819998f61e5cb698081f94782895e21b19
scuy <sebastian.cuy@uni-koeln.de>
2014-03-24 16:31:37 +0100

    added fake metadata index

---
+++ ContentBroker/src/main/java/de/uzk/hki/da/repository/FakeMetadataIndex.java
@@ -0,0 +1,14 @@
+package de.uzk.hki.da.repository;
+
+import java.util.Map;
+
+public class FakeMetadataIndex implements MetadataIndex {
+
+	@Override
+	public void indexMetadata(String indexName, String type, String id,
+			Map<String, Object> data) throws MetadataIndexException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
