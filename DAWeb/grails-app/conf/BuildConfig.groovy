34555a43aaef1ae80156157e9f70d0f68b9ab41d
jenspeters <peters@kcg.de>
2014-05-14 17:07:48 +0200

    Upgrade Grails to 2.3.8, changed ivy dependency resolving to maven

--- DAWeb/grails-app/conf/BuildConfig.groovy
+++ DAWeb/grails-app/conf/BuildConfig.groovy
@@ -4,15 +4,16 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
+grails.project.dependency.resolver="maven"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
+// inherit Grails' default dependencies
     inherits("global") {
         // uncomment to disable ehcache
         // excludes 'ehcache'
     }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    log "verbose" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
 
     repositories {
@@ -20,17 +21,20 @@ grails.project.dependency.resolution = {
         grailsPlugins()
         grailsHome()
         grailsCentral()
-        mavenCentral()
-
         // uncomment these to enable remote dependency resolution from public Maven repositories
         mavenCentral()
-        //mavenLocal()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
+        mavenLocal()
+	mavenRepo "http://repo.grails.org/grails/repo" 
+	mavenRepo "http://download.java.net/maven/2/"
+	mavenRepo "http://repository.jboss.org/nexus/content/groups/public-jboss/"
+	mavenRepo "http://mvnrepository.com/"
         //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-		mavenRepo "http://ci-dev.renci.org/nexus/content/repositories/public"
-    }
+        mavenRepo "http://download.java.net/maven/2/"
+        mavenRepo "http://repo.fusesource.com/nexus/content/groups/public" 
+	//mavenRepo "http://mvnrepository.com/artifact/"
+	mavenRepo "http://ci-dev.renci.org/nexus/content/repositories/public"
+    	mavenRepo "http://repo.grails.org/grails/plugins"
+	}
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 		// runtime 'mysql:mysql-connector-java:5.1.16'
@@ -51,28 +55,17 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.7.1"
+        runtime ":hibernate:3.6.10.2"
+	runtime ":jquery:1.7.1"
         runtime ":resources:1.1.6"
-		runtime('org.apache.activemq:activemq-all:5.8.0',
-			'org.apache.activemq:activeio-core:3.1.2',
-			'org.apache.xbean:xbean-spring:3.7') {
-			 excludes 'activemq-openwire-generator'
-			 excludes 'commons-logging'
-			 excludes 'xalan'
-			 excludes 'xml-apis'
-			 exported = false
-		   }
-	
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.4"
 
-        build ":tomcat:$grailsVersion"
-		compile ":jms:1.2"
-		
-	  compile ":jquery-ui:1.8.15" 
-	  compile ":modernizr:2.0.6"
+	build ":tomcat:7.0.42"	
+	compile ":jms:1.3"
+	compile ":jquery-ui:1.8.15" 
+	compile ":modernizr:2.0.6"
     }
 }
