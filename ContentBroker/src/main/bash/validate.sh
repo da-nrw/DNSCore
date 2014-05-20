ca4158d891c26508d1a3b13823611e3aa9018f6b
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2014-05-20 18:05:45 +0200

    fix path in check for already existing release candidate

--- ContentBroker/src/main/bash/validate.sh
+++ ContentBroker/src/main/bash/validate.sh
@@ -20,7 +20,7 @@ fi
 if [ "$1" = "ci" ]
 then
 	REVISION_NUMBER=`git rev-list HEAD | wc -l`
-	if [ -d "/data/danrw/buildRepository/installation.$REVISION_NUMBER" ]
+	if [ -d "/ci/BuildRepository/installation.$REVISION_NUMBER" ]
 	then
 		echo "The directory at /data/danrw/buildRepository/installation.$REVISION_NUMBER already exists which means"
 		echo "you already have a valid build for the current revision. Will exit now."
