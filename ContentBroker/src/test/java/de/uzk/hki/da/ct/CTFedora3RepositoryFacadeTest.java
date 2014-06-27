f0f20bba81ab0bc0644ca031276a28fd3408de97
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2014-06-27 20:09:34 +0200

    its done. basic accpetance test for rheinlaender works.

--- ContentBroker/src/test/java/de/uzk/hki/da/ct/CTFedora3RepositoryFacadeTest.java
+++ ContentBroker/src/test/java/de/uzk/hki/da/ct/CTFedora3RepositoryFacadeTest.java
@@ -12,12 +12,18 @@ import de.uzk.hki.da.repository.Fedora3RepositoryFacade;
 import de.uzk.hki.da.repository.RepositoryException;
 import de.uzk.hki.da.utils.PasswordUtils;
 
-public class Fedora3RepositoryFacadeTest {
+public class CTFedora3RepositoryFacadeTest {
 
 	private Fedora3RepositoryFacade fedora;
 
 	@Test
 	public void test() throws RepositoryException, IOException{
+		
+//		System.setProperty("http.proxySet","true");
+//        System.setProperty("http.proxyHost","localhost");
+//        System.setProperty("http.proxyPort","3126");   
+//        System.setProperty("http.proxyPassword","J9i64o21It");   
+//        System.setProperty("http.proxyUser","danrwproxy");   
 		
 		try {
 			fedora = new Fedora3RepositoryFacade("http://localhost:8080/fedora", "fedoraAdmin", PasswordUtils.decryptPassword("BYi/MFjKDFd5Dpe52PSUoA=="));
@@ -30,6 +36,7 @@ public class Fedora3RepositoryFacadeTest {
 		content = IOUtils.toString(fileInputStream, "UTF-8");
 		fileInputStream.close();
 		
+		fedora.purgeObjectIfExists("abc", "danrw");
 		fedora.createObject("abc", "danrw", "TEST");
 		
 		fedora.createMetadataFile("abc", "danrw", "ead123.xml", content, "label", "text/xml");
