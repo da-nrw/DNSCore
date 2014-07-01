5e84c232fc9c6f8e9e1c237999f03fa5c75664dc
jenspeters <peters@kcg.de>
2014-07-01 12:33:42 +0200

    removed old tests, refactored QueueEntry for unit testing

--- DAWeb/test/unit/daweb3/ObjectControllerTests.groovy
+++ DAWeb/test/unit/daweb3/ObjectControllerTests.groovy
@@ -19,38 +19,6 @@ class ObjectControllerTests {
         assert "/object/list" == response.redirectedUrl
     }
 
-    void testList() {
-
-		
-		def contractor = new Contractor(shortName: "rods", admin: 1)
-
-	
-		controller.session["contractor"] = contractor
-        def model = controller.list()
-
-        assert model.objectInstanceList.size() == 0
-        assert model.objectInstanceTotal == 0
-    }
-
-
-    void testShow() {
-        controller.show()
-
-        assert flash.message != null
-        assert response.redirectedUrl == '/object/list'
-
-
-        populateValidParams(params)
-        def object = new Object(params)
-
-        assert object.save() != null
-
-        params.id = object.id
-
-        def model = controller.show()
-
-        assert model.objectInstance == object
-    }
 
    
 }
--- DAWeb/test/unit/daweb3/QueueEntryControllerTests.groovy
+++ DAWeb/test/unit/daweb3/QueueEntryControllerTests.groovy
@@ -21,31 +21,4 @@ class QueueEntryControllerTests {
         assert "/queueEntry/list" == response.redirectedUrl
     }
 
-    void testList() {
-
-        def model = controller.list()
-
-        assert model.queueEntryInstanceList.size() == 0
-        assert model.queueEntryInstanceTotal == 0
-    }
-
-    void testShow() {
-        controller.show()
-
-        assert flash.message != null
-        assert response.redirectedUrl == '/queueEntry/list'
-
-
-        populateValidParams(params)
-        def queueEntry = new QueueEntry(params)
-
-        assert queueEntry.save() != null
-
-        params.id = queueEntry.id
-
-        def model = controller.show()
-
-        assert model.queueEntryInstance == queueEntry
-    }
-
 }
