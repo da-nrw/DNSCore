da4abff180428fa7563300ce70678d5ab1d25c55
PolinaGubaidullina <polinagubai@web.de>
2014-07-18 16:34:48 +0200

    adjusted mimetype location in target mets file

--- ContentBroker/src/main/java/de/uzk/hki/da/service/MimeTypeDetectionService.java
+++ ContentBroker/src/main/java/de/uzk/hki/da/service/MimeTypeDetectionService.java
@@ -14,6 +14,9 @@ public class MimeTypeDetectionService{
 	protected static Logger logger = LoggerFactory.getLogger(MimeTypeDetectionService.class);
 
 	public String detectMimeType(DAFile dafile) throws IOException {
+		
+		logger.debug("Detect MIMETYPE ");
+		
 		File file = dafile.toRegularFile();
 		String mimeType;
 
@@ -23,7 +26,8 @@ public class MimeTypeDetectionService{
 	        logger.debug("Detected MIME type {} for file {}",mimeType,file.getName());
 	    }  catch (IOException e) {
 	        throw new IOException("Unable to open file for mime type detection: " + file.getAbsolutePath(), e);
-	    }		
+	    }	
+	    logger.debug("return "+mimeType);
 		return mimeType;	
 	}
 }
