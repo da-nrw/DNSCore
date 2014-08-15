7baa54bd31f7bd224c7e78473e9c3fb02ceccfd8
jenspeters <peters@kcg.de>
2014-08-15 08:46:18 +0200

    new Role Class added

---
+++ ContentBroker/src/main/java/de/uzk/hki/da/model/Role.java
@@ -0,0 +1,56 @@
+/*
+  DA-NRW Software Suite | ContentBroker
+  Copyright (C) 2013 Historisch-Kulturwissenschaftliche Informationsverarbeitung
+  Universität zu Köln
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package de.uzk.hki.da.model;
+import javax.persistence.*;
+
+
+/**
+ * The Class Role.
+ */
+@Entity
+@Table(name="role")
+public class Role{
+	
+	/** The id. */
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	/** The authority */
+	private String authority;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+	
+}
