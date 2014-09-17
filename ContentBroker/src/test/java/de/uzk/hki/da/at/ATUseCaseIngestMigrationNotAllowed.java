c9f083446ef1bce0ce305ea6743233eec88b76e5
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2014-09-17 15:02:22 +0200

    Refactor acceptance tests in preparation for proper implementation of atusecaseingestmigrationnotallowed. Retrieve package must be fed with param targetFolder so that callers know which file gets created. so they see it better which files to delete during the teardown test phases.

---
+++ ContentBroker/src/test/java/de/uzk/hki/da/at/ATUseCaseIngestMigrationNotAllowed.java
@@ -0,0 +1,59 @@
+/*
+  DA-NRW Software Suite | ContentBroker
+  Copyright (C) 2014 LVRInfoKom
+  Landschaftsverband Rheinland
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package de.uzk.hki.da.at;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.uzk.hki.da.test.TESTHelper;
+import de.uzk.hki.da.model.Object;
+
+/**
+ * @author Daniel M. de Oliveira
+ */
+public class ATUseCaseIngestMigrationNotAllowed extends Base {
+
+	private static final File UNPACKED_DIP = new File("/tmp/MigrationUnpacked");
+	private static final String ORIG_NAME = "ATMigrationAllowed";
+	
+	@Before
+	public void setUp() throws IOException{
+		setUpBase();
+	}
+	
+	@After
+	public void tearDown(){
+
+		FileUtils.deleteQuietly(UNPACKED_DIP);
+		TESTHelper.clearDB();
+		cleanStorage();
+	}
+	
+	@Test
+	public void test() throws IOException{
+		Object o = ingest(ORIG_NAME);
+		retrievePackage(o, UNPACKED_DIP, "1");
+	}
+}
