35f915e5bccca69132fe7ee00e77e91dcc8708ab
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2014-09-19 17:56:16 +0200

    rename method to field in annotation parser.

--- ContentBroker/src/test/java/de/uzk/hki/da/cb/ActionUnderTestAnnotationParser.java
+++ ContentBroker/src/test/java/de/uzk/hki/da/cb/ActionUnderTestAnnotationParser.java
@@ -6,9 +6,9 @@ public class ActionUnderTestAnnotationParser {
 
 	   public Field parse(Class<?> clazz) throws Exception {
 	      Field[] fields = clazz.getDeclaredFields();
-	      for (Field method : fields) {
-	         if (method.isAnnotationPresent(ActionUnderTest.class)) {
-	        	 return method;
+	      for (Field field : fields) {
+	         if (field.isAnnotationPresent(ActionUnderTest.class)) {
+	        	 return field;
 	         }
 	      }
 	      return null;
