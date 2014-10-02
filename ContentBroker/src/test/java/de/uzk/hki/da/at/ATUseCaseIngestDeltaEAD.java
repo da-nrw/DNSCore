5ef02114d0bbb15da13781d19b78337d1c4e04b0
PolinaGubaidullina <polinagubai@web.de>
2014-10-02 13:38:21 +0200

    Bugfix

--- ContentBroker/src/test/java/de/uzk/hki/da/at/ATUseCaseIngestDeltaEAD.java
+++ ContentBroker/src/test/java/de/uzk/hki/da/at/ATUseCaseIngestDeltaEAD.java
@@ -30,7 +30,7 @@ public class ATUseCaseIngestDeltaEAD extends AcceptanceTest{
 		FileUtils.deleteQuietly(new File("src/test/resources/at/"+ORIG_NAME_ORIG+".tgz"));
 		
 		Thread.sleep(_1_MINUTE); // to prevent the repnames to match the ones of the previous package
-		FileUtils.copyFileToDirectory(new File("src/test/resources/at/"+ORIG_NAME_ORIG+"_delta_oneFile/"+ORIG_NAME_ORIG+".tgz"), new File("src/test/resources/at"));
+		FileUtils.copyFileToDirectory(new File("src/test/resources/at/"+ORIG_NAME_ORIG+"_delta/"+ORIG_NAME_ORIG+".tgz"), new File("src/test/resources/at"));
 		object = ath.ingest(ORIG_NAME_ORIG);
 		FileUtils.deleteQuietly(new File("src/test/resources/at/"+ORIG_NAME_ORIG+".tgz"));
 	}
