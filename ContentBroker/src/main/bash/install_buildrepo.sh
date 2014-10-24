193585bc9e7ef3a3c4ab8e798b9ae2d5bf2d1ac2
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2014-10-24 15:31:56 +0200

    unite sip builder install-buildrepo.sh and dnscore install-buildrepo.sh.

--- ContentBroker/src/main/bash/install_buildrepo.sh
+++ ContentBroker/src/main/bash/install_buildrepo.sh
@@ -8,6 +8,7 @@ TARGET=/ci/BuildRepository/installation.$REVISION_NUMBER
 mkdir $TARGET
 mkdir $TARGET/ContentBroker
 mkdir $TARGET/DAWeb
+mkdir $TARGET/SipBuilder
 
 cd /ci/DNSCore/ContentBroker
 cp -r target/installation $TARGET/ContentBroker
@@ -15,3 +16,6 @@ cp -r target/installation $TARGET/ContentBroker
 cd /ci/DNSCore/DAWeb
 cp target/daweb3-0.8.war $TARGET
 cp doc/daweb3_properties.groovy.dev $TARGET/DAWeb/daweb3_properties.groovy.template
+
+cd /ci/DNSCore/SIP-Builder
+cp -r target/installation $TARGET/SipBuilder
--- SIP-Builder/src/main/bash/install_buildrepo.sh
+++
@@ -1,7 +0,0 @@
-!/bin/bash
-
-# author: Daniel M. de Oliveira
-
-cd /ci/DNSCore/ContentBroker
-REVISION_NUMBER=`git rev-parse HEAD`
-cp -r target/installation /ci/BuildRepositorySIPBuilder/installation.$REVISION_NUMBER
