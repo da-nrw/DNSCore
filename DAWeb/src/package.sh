ace9dbe5a9d39aef02c6c245863c7d1a7acb9eb1
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2014-10-24 15:00:19 +0200

    fix package.sh missing revision number.

--- DAWeb/src/package.sh
+++ DAWeb/src/package.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 VERSION="v`cat ../VERSION.txt` (build: `echo $BUILD_NUMBER` rev: `git rev-parse HEAD` created on `date`)"
+REVISION_NUMBER=`git rev-parse HEAD`
+TARGET=/ci/BuildRepository/installation.$REVISION_NUMBER/DAWeb
 
 echo "copying Version and Build Info to DaWeb"
 cp -f application.properties.template application.properties
 mv application.properties application.properties.tmp
 sed "s@app\.version=.*@app\.version=$VERSION@" application.properties.tmp >> application.properties
 rm application.properties.tmp
-
-TARGET=/ci/BuildRepository/installation.$REVISION_NUMBER/DAWeb
 
 mkdir $TARGET
 cp target/daweb3-0.8.war $TARGET
