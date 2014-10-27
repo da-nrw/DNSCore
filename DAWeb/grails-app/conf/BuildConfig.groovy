65fa998b69e7bcf3d77146d18d887ed57fe0c4eb
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2014-10-27 12:55:14 +0100

    try to fix git problem.

--- DAWeb/grails-app/conf/BuildConfig.groovy
+++ DAWeb/grails-app/conf/BuildConfig.groovy
@@ -1,84 +1,95 @@
-grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
+grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-grails.project.work.dir = "target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
+grails.project.dependency.resolver="maven"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
-grails.project.fork = [
-    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
-    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-
-    // configure settings for the test-app JVM, uses the daemon by default
-    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-    // configure settings for the run-app JVM
-    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the run-war JVM
-    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the Console UI JVM
-    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
-]
-
-grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
+	pom true
+	legacyResolve true
     inherits("global") {
-        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
+    	excludes 'spring-asm'
+    	excludes "slf4j-api"
+    	excludes "slf4j-log4j12"
+    	excludes "jul-to-slf4j"
+    	excludes "jcl-over-slf4j"
+    	
+        // uncomment to disable ehcache
         // excludes 'ehcache'
+        //exclude group: 'org.slf4j', module:'jcl-over-slf4j'
+       // exclude group: 'org.slf4j', module:'jul-to-slf4j'
+        //exclude group: 'org.slf4j', module:'slf4j-api'
+        //exclude group: 'org.slf4j', module:'slf4j-simple'
     }
+<<<<<<< HEAD
 
 
     //log "debug" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+=======
+    log "debug" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+>>>>>>> parent of 58a28b3... set build number into application properties with new script daweb3/src/package.sh
     checksums true // Whether to verify checksums on resolve
-    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
     repositories {
         inherits true // Whether to inherit repository definitions from plugins
-
         grailsPlugins()
         grailsHome()
-        mavenLocal()
         grailsCentral()
+        // uncomment these to enable remote dependency resolution from public Maven repositories
         mavenCentral()
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
+        mavenLocal()
+		mavenRepo 'http://repo.spring.io/milestone'
+	mavenRepo "http://repo.grails.org/grails/repo" 
+	mavenRepo "http://download.java.net/maven/2/"
+	mavenRepo "http://repository.jboss.org/nexus/content/groups/public-jboss/"
+	mavenRepo "http://mvnrepository.com/"
         //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
-
+        mavenRepo "http://download.java.net/maven/2/"
+        mavenRepo "http://repo.fusesource.com/nexus/content/groups/public" 
+	//mavenRepo "http://mvnrepository.com/artifact/"
+	//mavenRepo "http://ci-dev.renci.org/nexus/content/repositories/public"
+    	mavenRepo "http://repo.grails.org/grails/plugins"
+	}
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-        // runtime 'mysql:mysql-connector-java:5.1.27'
-        // runtime 'org.postgresql:postgresql:9.3-1100-jdbc41'
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+		// runtime 'mysql:mysql-connector-java:5.1.16'
+		runtime 'postgresql:postgresql:9.0-801.jdbc4'
+		runtime 'org.hsqldb:hsqldb:2.3.1'
+		compile('org.apache.activemq:activemq-client:5.9.0',
+			'org.apache.xbean:xbean-spring:3.7') {
+			 excludes 'activemq-openwire-generator'
+			 excludes 'commons-logging'
+			 excludes "spring-context"
+			 excludes 'xalan'
+			 excludes 'slf4j-api'
+			 excludes 'org.slf4j'
+			
+			 excludes 'xml-apis'
+			 exported = false
+		}
+		//compile('org.irods.jargon:jargon-core:3.3.2-beta1') {
+		//	excludes "slf4j-api", "slf4j-log4j12", "commons-io", "commons-codec","cog-jglobus"
+		//}
     }
 
     plugins {
-        // plugins for the build system only
-        build ":tomcat:7.0.52.1"
-
-        // plugins for the compile step
-        compile ":scaffolding:2.0.3"
-        compile ':cache:1.1.2'
-
-        // plugins needed at runtime but not for compilation
-        runtime ":hibernate:3.6.10.13" // or ":hibernate4:4.3.5.1"
-        runtime ":database-migration:1.4.0"
-        runtime ":jquery:1.11.0.2"
-        runtime ":resources:1.2.7"
+        runtime ":hibernate:3.6.10.2"
+		runtime ":jquery:1.7.1"
+        runtime ":resources:1.1.6"
         // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0.1"
-        //runtime ":cached-resources:1.1"
-        //runtime ":yui-minify-resources:0.1.5"
-
-        // An alternative to the default resources plugin is the asset-pipeline plugin
-        //compile ":asset-pipeline:1.6.1"
-
-        // Uncomment these to enable additional asset-pipeline capabilities
-        //compile ":sass-asset-pipeline:1.5.5"
-        //compile ":less-asset-pipeline:1.5.3"
-        //compile ":coffee-asset-pipeline:1.5.0"
-        //compile ":handlebars-asset-pipeline:1.3.0.1"
+        //runtime ":zipped-resources:1.0"
+        //runtime ":cached-resources:1.0"
+        //runtime ":yui-minify-resources:0.1.4"
+		compile ":spring-security-core:2.0-RC4"
+		compile ":scaffolding:2.0.1"
+		build ":tomcat:7.0.42"	
+		compile (":jms:1.3") {
+		excludes 'spring-asm'
+		}
+		compile ":jquery-ui:1.8.15" 
+		compile ":modernizr:2.0.6"
     }
 }
