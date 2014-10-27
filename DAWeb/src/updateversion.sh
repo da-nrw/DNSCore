df1acd1431f0e67f3ca39ddf3080a2c312d7566d
jenspeters <peters@kcg.de>
2014-10-27 15:46:25 +0100

    format

--- DAWeb/src/updateversion.sh
+++ DAWeb/src/updateversion.sh
@@ -2,8 +2,6 @@
 
 VERSION="v`cat ../VERSION.txt` (build: `echo $BUILD_NUMBER` rev: `git rev-parse HEAD` created on `date`)"
 
-
-
 echo "copying Version and Build Info to DaWeb"
 cp -f application.properties.template application.properties
 mv application.properties application.properties.tmp
