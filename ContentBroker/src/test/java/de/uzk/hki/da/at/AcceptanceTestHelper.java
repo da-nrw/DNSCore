6fc4309ce3ec9d14b88db4bb42e999cc36443be1
jenspeters <peters@kcg.de>
2014-10-28 13:10:21 +0100

    setting timeout 500000

--- ContentBroker/src/test/java/de/uzk/hki/da/at/AcceptanceTestHelper.java
+++ ContentBroker/src/test/java/de/uzk/hki/da/at/AcceptanceTestHelper.java
@@ -56,7 +56,7 @@ public class AcceptanceTestHelper {
 	protected static Path TEST_DATA_ROOT_PATH = new RelativePath("src/test/resources/at/");
 	
 	private static final int INTERVAL=2000; // in ms
-	private static final int TIMEOUT=300000; // ins ms
+	private static final int TIMEOUT=500000; // ins ms
 	
 	private GridFacade gridFacade;
 	private Node localNode;
