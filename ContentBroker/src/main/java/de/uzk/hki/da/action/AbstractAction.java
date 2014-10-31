bb3fef76bfcd2ece77937c3acd868beabcae6615
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2014-10-31 14:21:31 +0100

    do the derigister the proper way.

--- ContentBroker/src/main/java/de/uzk/hki/da/action/AbstractAction.java
+++ ContentBroker/src/main/java/de/uzk/hki/da/action/AbstractAction.java
@@ -151,25 +151,22 @@ public abstract class AbstractAction implements Runnable {
 		
 		try {
 			execAndPostProcessImplementation();
-			actionMap.deregisterAction(this); // now the action does't block resources anymore. free resources as soon as possible.
 			upateObjectAndJob(object,job,isDELETEOBJECT(),KILLATEXIT,toCreate);
 			
 		} catch (UserException e) {
 			
 			execAndPostProcessRollback(object,job,C.WORKFLOW_STATE_DIGIT_USER_ERROR);
-			actionMap.deregisterAction(this); // see above
 			upateObjectAndJob(object, job, false, false, null);
 			reportUserError(e);
 			
 		} catch (Exception e) {
 			
 			execAndPostProcessRollback(object,job,"1");
-			actionMap.deregisterAction(this); // see above
 			upateObjectAndJob(object, job, false, false, null);
 			reportTechnicalError(e);
-			
 		} 		
 			
+		actionMap.deregisterAction(this); // now the action does't block resources anymore.
 		unsetObjectLogging();
 	}
 
