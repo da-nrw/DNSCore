c1fb12efc94deee75eed2106018446b22bf55bb1
PolinaGubaidullina <polinagubai@web.de>
2015-01-15 11:40:36 +0100

    modified pre-integration-test.sh

--- ContentBroker/src/main/bash/pre-integration-test.sh
+++ ContentBroker/src/main/bash/pre-integration-test.sh
@@ -136,12 +136,6 @@ src/main/bash/populatetestdb.sh populate $1
 
 
 ## remove some stuff so that the installer can make the real files out of the template files again
-rm $INSTALL_PATH/conf/beans.xml
-rm $INSTALL_PATH/conf/config.properties
-rm $INSTALL_PATH/conf/hibernateCentralDB.cfg.xml
-rm $INSTALL_PATH/conf/logback.xml
-rm $INSTALL_PATH/ContentBroker_start.sh
-rm $INSTALL_PATH/ContentBroker_stop.sh
 rm $INSTALL_PATH/conf/beans.xml > /dev/null
 rm $INSTALL_PATH/conf/config.properties > /dev/null
 rm $INSTALL_PATH/conf/hibernateCentralDB.cfg.xml > /dev/null
@@ -150,6 +144,8 @@ rm $INSTALL_PATH/ContentBroker_start.sh > /dev/null
 rm $INSTALL_PATH/ContentBroker_stop.sh > /dev/null
 m $INSTALL_PATH/log/contentbroker.log
 > $INSTALL_PATH/log/contentbroker.log
+
+>>>>>>> parent of 3994129... New ATs: Unreferenced Files
 
 install $INSTALL_PATH $BEANS
 sed -i "s/INFO/DEBUG/g" $INSTALL_PATH/conf/logback.xml
