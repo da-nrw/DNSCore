f327fa36bd6b910c4a47509defd818b0f914bf5d
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2015-01-22 13:45:47 +0100

    clean up maven lifecycle invoked scripts.

--- ContentBroker/src/main/bash/pre-integration-test.sh
+++ ContentBroker/src/main/bash/pre-integration-test.sh
@@ -64,18 +64,7 @@ function install(){
 }
 
 function launchXDB(){
-	DATABASE_PROC_ID=`ps -aef | grep hsqldb.jar | grep -v grep | awk '{print $2}'`
-	if [ "$DATABASE_PROC_ID" != "" ]
-	then
-	        echo Killing hsql database process $DATABASE_PROC_ID.
-	        kill -9 $DATABASE_PROC_ID
-	fi
-	
-	sleep 2
-	
 	echo Recreating da-nrw schema in hsql database.
-	rm -r mydb.tmp 2> /dev/null
-	rm mydb.*      2> /dev/null
 	
 	cd $HIER
 	java -cp ../3rdParty/hsqldb/lib/hsqldb.jar org.hsqldb.server.Server --database.0 file:mydb --dbname.0 xdb &
