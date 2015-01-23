c03f3a43c9fa2990c44b389b514f342c761cb7e8
jenspeters <peters@kcg.de>
2015-01-23 09:55:47 +0100

    synchronizing service for AIPs

--- ContentBroker/src/main/rules/irodsFederatedGridFacade/synchronize.r
+++ ContentBroker/src/main/rules/irodsFederatedGridFacade/synchronize.r
@@ -3,9 +3,7 @@
 # Author: Jens Peters
 # This file is licenced under the GPLv3 as the main core of DNS, too.
 #
-# DA-NRW Software Suite
-#  Copyright (C) 2013 Historisch-Kulturwissenschaftliche Informationsverarbeitung
-#  Universität zu Köln, 2014 LVR InfoKom
+# DA-NRW Software Suite 2014 LVR InfoKom
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -20,23 +18,30 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 synchronizeService {
-	#delay("<PLUSET>1m</PLUSET><EF>1m</EF>") {
+	delay("<PLUSET>1m</PLUSET><EF>2h</EF>") {
 	acLog("---started Synchronize Service---");
-	msiExecStrCondQuery("SELECT DATA_NAME, COLL_NAME, META_DATA_ATTR_NAME, META_DATA_ATTR_VALUE where COLL_NAME like '/*homezone/aip/%' and META_DATA_ATTR_NAME = 'FEDERATED' and META_DATA_ATTR_VALUE = '0'",*colls)
+	msiGetSystemTime(*timeNow,"")
+	*ts=(*retryOlderThanHours*3600)
+	msiExecStrCondQuery("SELECT DATA_NAME,DATA_MODIFY_TIME, COLL_NAME, META_DATA_ATTR_NAME, META_DATA_ATTR_VALUE where COLL_NAME like '/*homezone/aip/%' and META_DATA_ATTR_NAME = 'FEDERATED' and META_DATA_ATTR_VALUE = '0'",*colls)
 	foreach(*colls) {
         msiGetValByKey(*colls,"COLL_NAME",*srcColl);
 		msiGetValByKey(*colls,"DATA_NAME",*dao);
-        msiSplitPath(*srcColl,*cColl,*objId)
+        	msiSplitPath(*srcColl,*cColl,*objId)
 		msiSplitPath(*cColl,*par,*chil)
 		msiSplitPath(*par,*par,*aip)
 		if (*aip=="aip") {
-			federateObject(*srcColl,*dao,*destResc,*min_copies)
+			msiGetValByKey(*colls,"DATA_MODIFY_TIME",*dmt)
+			msiGetDiffTime(*dmt,*timeNow,"",*et)
+			if (int(*et)>*ts) {
+				acLog("Retry synchronizing *srcColl/*dao last attempt is older than *retryOlderThanHours Hours in the past")
+				federateObject(*srcColl,*dao,*destResc,*min_copies)
+			}
 		}
 
 	}
 	acLog("---Ended Synchronize Service---");
-#}
 }
-INPUT *destResc=$"lza", *homezone=$"lvr", *min_copies=$3
+}
+INPUT *destResc=$"lza", *homezone=$"lvr", *min_copies=$3, *retryOlderThanHours=$24
 OUTPUT ruleExecOut
 
