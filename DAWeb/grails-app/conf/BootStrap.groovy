1f874e3822aa54e1ffb5ec5140c125759a4f71dc
jenspeters <peters@kcg.de>
2015-01-23 15:28:11 +0100

    Revert "dev is default env for mvn scripts. appHome param is obsolete. running CB instance gets placed at target/ContentBroker"
    This reverts commit 1406f23bf9b7704e46f5539a538ec0ecbe5fe779.

--- DAWeb/grails-app/conf/BootStrap.groovy
+++ DAWeb/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,32 @@
+import daweb3.User
+import daweb3.Role
+import daweb3.UserRole
+import grails.util.Environment
 class BootStrap {
 
     def init = { servletContext ->
-    }
+		
+		if (Environment.current == Environment.DEVELOPMENT) {	
+			println "BOOTSTRAPPED DEV Environment activated"
+			User testuser = User.findByUsername('rods')
+			testuser.setPassword("rods")
+			if (!testuser.save(flush: true)) {
+				println "Error updating Admin DEV User!"
+				testuser.errors.allErrors.each {
+					println it
+				}
+			}
+			testuser = User.findByUsername('TEST')
+			testuser.setPassword("TEST")
+			if (!testuser.save(flush: true)) {
+				println "Error updating TEST DEV User!"
+				testuser.errors.allErrors.each {
+					println it
+				}
+			}
+
+		}
+	}
     def destroy = {
     }
 }
--- DAWeb/grails-app/conf/spring/resources.groovy
+++ DAWeb/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,6 @@
-// Place your Spring DSL code here
+import grails.util.*
 beans = {
+  jmsConnectionFactory(org.apache.activemq.ActiveMQConnectionFactory) {
+    brokerURL = 'tcp://localhost:'+ application.config.cb.port
+  }
 }
