1559986ce2256b5fd864f67ffe8e85b9b7433354
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2015-01-28 12:36:31 +0100

    implement rollback and test it.

--- ContentBroker/src/main/java/de/uzk/hki/da/cb/FetchPIPsAction.java
+++ ContentBroker/src/main/java/de/uzk/hki/da/cb/FetchPIPsAction.java
@@ -21,15 +21,17 @@
 
 package de.uzk.hki.da.cb;
 
+import static de.uzk.hki.da.core.C.WA_INSTITUTION;
+import static de.uzk.hki.da.core.C.WA_PIPS;
+import static de.uzk.hki.da.core.C.WA_PUBLIC;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.NotImplementedException;
 
 import de.uzk.hki.da.action.AbstractAction;
-import static de.uzk.hki.da.core.C.*;
 import de.uzk.hki.da.core.IngestGate;
 import de.uzk.hki.da.grid.DistributedConversionAdapter;
 import de.uzk.hki.da.util.ConfigurationException;
@@ -40,40 +42,57 @@ import de.uzk.hki.da.util.Path;
  * Overwrites any previously generated PIPs.
  * 
  * @author Daniel M. de Oliveira
+ * @author Jens Peters
  */
 public class FetchPIPsAction extends AbstractAction {
 
-	private IngestGate ingestGate;
-	
+	private static final String INFO_MSG_MOVED_BACK_PIP = "Moved back PIP: ";
+	private static final String INFO_MSG_REPLICATED_SUCESSFULLY = "Replicated sucessfully: ";
+	private static final String ERR_MSG_DISTRIBUTED_CONVERSION_ADAPTER_NOT_SET = "DistributedConversionAdapter not set";
+	private static final String UNDERSCORE = "_";
+	private IngestGate ingestGate; /*unused*/
 	private DistributedConversionAdapter distributedConversionAdapter;
-	
-	Path publicContractorFolder = null;
-	Path institutionContractorFolder = null;
 	
 	@Override
 	public void checkActionSpecificConfiguration() throws ConfigurationException {
-		if (distributedConversionAdapter==null) throw new ConfigurationException("distributedConversionAdapter not set");
+		if (distributedConversionAdapter==null) throw new ConfigurationException(ERR_MSG_DISTRIBUTED_CONVERSION_ADAPTER_NOT_SET);
 	}
 
 
 
 	@Override
-	public void checkSystemStatePreconditions() throws IllegalStateException {
-		// Auto-generated method stub
-	}
+	public void checkSystemStatePreconditions() throws IllegalStateException {/*none*/}
 
 
 
 	@Override
 	public boolean implementation() throws FileNotFoundException, IOException {
+
+		deletePreviousPIPs(); // Must be done at the beginning to make sure rollback() can act properly
 		
-		replicateFromSourceResourceToWorkingResource();
-		// the rename is necessary because at the moment we donl't have another possibility to delete or trim the irods
-		// collections on specific resources.
-		deletePreviousPIPs();
-		renamePIPs();
+		distributedConversionAdapter.replicateToLocalNode(
+				makePIPSourceFolder(WA_PUBLIC).toString());
+		distributedConversionAdapter.replicateToLocalNode(
+				makePIPSourceFolder(WA_INSTITUTION).toString());
 		
-		// cleanup
+		
+		if (makePIPSourceFolder(WA_PUBLIC).exists()) {
+			logger.info(INFO_MSG_REPLICATED_SUCESSFULLY+makePIPSourceFolder(WA_PUBLIC));
+
+//          The rename is necessary because at the moment we donl't have another possibility to delete or trim the irods
+//          collections on specific resources.
+			FileUtils.moveDirectory(
+					makePIPSourceFolder(WA_PUBLIC), 
+					makePIPFolder(WA_PUBLIC));
+		}
+		if (makePIPSourceFolder(WA_INSTITUTION).exists()) {
+			logger.info(INFO_MSG_REPLICATED_SUCESSFULLY+makePIPSourceFolder(WA_INSTITUTION));
+			
+			FileUtils.moveDirectory(
+					makePIPSourceFolder(WA_INSTITUTION), 
+					makePIPFolder(WA_INSTITUTION));
+		}
+		
 		distributedConversionAdapter.remove(makePIPSourceFolder(WA_PUBLIC).toString());
 		distributedConversionAdapter.remove(makePIPSourceFolder(WA_INSTITUTION).toString());
 
@@ -82,7 +101,21 @@ public class FetchPIPsAction extends AbstractAction {
 
 	@Override
 	public void rollback() throws Exception {
-		throw new NotImplementedException("No rollback implemented for this action");
+		
+		if (makePIPFolder(WA_PUBLIC).exists()) {
+			logger.info(INFO_MSG_MOVED_BACK_PIP+makePIPSourceFolder(WA_PUBLIC));
+
+			FileUtils.moveDirectory(
+					makePIPFolder(WA_PUBLIC), 
+					makePIPSourceFolder(WA_PUBLIC)); // we know that this is possible to write to this location since we cleared it at beginning of implementation() 
+		}
+		if (makePIPFolder(WA_INSTITUTION).exists()) {
+			logger.info(INFO_MSG_MOVED_BACK_PIP+makePIPSourceFolder(WA_INSTITUTION));
+			
+			FileUtils.moveDirectory(
+					makePIPFolder(WA_INSTITUTION), 
+					makePIPSourceFolder(WA_INSTITUTION));
+		}
 	}
 
 	private File makePIPFolder(String pipType) {
@@ -90,7 +123,7 @@ public class FetchPIPsAction extends AbstractAction {
 	}
 	
 	private File makePIPSourceFolder(String pipType) {
-		return Path.makeFile(n.getWorkAreaRootPath(),WA_PIPS,pipType,o.getContractor().getShort_name(),o.getIdentifier()+"_"+o.getLatestPackage().getId());
+		return Path.makeFile(n.getWorkAreaRootPath(),WA_PIPS,pipType,o.getContractor().getShort_name(),o.getIdentifier()+UNDERSCORE+o.getLatestPackage().getId());
 	}
 	
 	
@@ -106,48 +139,6 @@ public class FetchPIPsAction extends AbstractAction {
 	}
 
 
-
-	/**
-	 * @param sourceDIPName
-	 * @param targetDIPName
-	 * @throws IOException
-	 */
-	private void renamePIPs() throws IOException {
-		
-		logger.debug("Rename PIP Public " +makePIPSourceFolder(WA_PUBLIC).toString()  + " to "  + makePIPFolder(WA_PUBLIC).toString() );
-		logger.debug("Rename PIP Institution " +makePIPSourceFolder(WA_INSTITUTION).toString()  + " to "  + makePIPFolder(WA_INSTITUTION).toString() );
-		
-		if (makePIPSourceFolder(WA_PUBLIC).exists()) {
-			FileUtils.moveDirectory(
-					makePIPSourceFolder(WA_PUBLIC), 
-					makePIPFolder(WA_PUBLIC));
-		} else logger.debug(makePIPSourceFolder(WA_PUBLIC).toString()+ " does not exist, could not perform rename!" );
-		if (makePIPSourceFolder(WA_INSTITUTION).exists()) {
-			FileUtils.moveDirectory(
-					makePIPSourceFolder(WA_INSTITUTION), 
-					makePIPFolder(WA_INSTITUTION));
-		} else logger.debug(makePIPSourceFolder(WA_INSTITUTION).toString()+ " does not exist, could not perform rename!" );
-	}
-
-	
-	
-	
-
-
-	/**
-	 * @author Daniel M. de Oliveira
-	 * @author Jens Peters
-	 * @param dipSourcePartialPath
-	 */
-	private void replicateFromSourceResourceToWorkingResource() {
-//		TODO check if source exists
-			distributedConversionAdapter.replicateToLocalNode(
-					makePIPSourceFolder(WA_PUBLIC).toString());
-//		TODO check if source exists
-			 distributedConversionAdapter.replicateToLocalNode(
-					 makePIPSourceFolder(WA_INSTITUTION).toString());
-	}
-	
 	
 	public DistributedConversionAdapter getDistributedConversionAdapter() {
 		return distributedConversionAdapter;
--- ContentBroker/src/test/java/de/uzk/hki/da/cb/FetchPIPsActionTest.java
+++ ContentBroker/src/test/java/de/uzk/hki/da/cb/FetchPIPsActionTest.java
@@ -19,9 +19,11 @@
 
 package de.uzk.hki.da.cb;
 
+import static de.uzk.hki.da.core.C.FILE_EXTENSION_XML;
 import static de.uzk.hki.da.core.C.WA_INSTITUTION;
 import static de.uzk.hki.da.core.C.WA_PIPS;
 import static de.uzk.hki.da.core.C.WA_PUBLIC;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
@@ -47,9 +49,12 @@ import de.uzk.hki.da.util.RelativePath;
 public class FetchPIPsActionTest extends ConcreteActionUnitTest {
 	
 
+
 	@ActionUnderTest
 	static FetchPIPsAction action = new FetchPIPsAction();
 
+	private static final String PREMIS = "premis";
+	private static final String _140849_TIF = "140849.tif";
 	private static final Path TESTDIR = new RelativePath(TC.TEST_ROOT_CB, "FetchPIPsAction");
 	private static final String UNDERSCORE = "_";
 	private static DistributedConversionAdapter distributedConversionAdapter;
@@ -75,15 +80,67 @@ public class FetchPIPsActionTest extends ConcreteActionUnitTest {
 	
 	
 	@Test
-	public void renamePIPs() throws FileNotFoundException, IOException{
+	public void overwriteExistingPIPs() throws FileNotFoundException, IOException{
+		assertFalse(makeMetadataFile(PREMIS,WA_PUBLIC).exists());
+		assertFalse(makeMetadataFile(PREMIS,WA_INSTITUTION ).exists());
 		action.implementation();
-		assertTrue(makePIPFolder( WA_PUBLIC ).exists());
-		assertTrue(makePIPFolder( WA_INSTITUTION ).exists());
+		assertTrue(makeMetadataFile(PREMIS,WA_PUBLIC).exists());
+		assertTrue(makeMetadataFile(PREMIS,WA_INSTITUTION ).exists());
+	}
+	
+	@Test
+	public void movePIPs() throws FileNotFoundException, IOException {
+		action.implementation();
+		assertFalse(makePIPSourceFolder(WA_PUBLIC).exists());
+		assertFalse(makePIPSourceFolder(WA_INSTITUTION).exists());
+		assertTrue(makePIPFolder(WA_PUBLIC).exists());
+		assertTrue(makePIPFolder(WA_INSTITUTION).exists());
+	}
+	
+	@Test
+	public void rollback() throws Exception {
+		simulateImpl();
+		action.rollback();
+		assertTrue(makeFileInSrcFolder(_140849_TIF, WA_PUBLIC).exists());
+		assertTrue(makeFileInSrcFolder(_140849_TIF, WA_INSTITUTION).exists());
+		assertTrue(makeFileInSrcFolder(PREMIS+FILE_EXTENSION_XML, WA_PUBLIC).exists());
+		assertTrue(makeFileInSrcFolder(PREMIS+FILE_EXTENSION_XML, WA_INSTITUTION).exists());
+		assertFalse(makePIPFolder(WA_PUBLIC).exists());
+		assertFalse(makePIPFolder(WA_INSTITUTION).exists());
+	}
+
+
+	private void simulateImpl() throws IOException {
+		FileUtils.deleteDirectory(makePIPFolder(WA_PUBLIC));
+		FileUtils.deleteDirectory(makePIPFolder(WA_INSTITUTION));
+		FileUtils.moveDirectory(
+				makePIPSourceFolder(WA_PUBLIC), 
+				makePIPFolder(WA_PUBLIC));
+		FileUtils.moveDirectory(
+				makePIPSourceFolder(WA_INSTITUTION), 
+				makePIPFolder(WA_INSTITUTION));
+		FileUtils.deleteDirectory(makePIPSourceFolder(WA_PUBLIC));
+		FileUtils.deleteDirectory(makePIPSourceFolder(WA_INSTITUTION));
 	}
 	
 	
 	
+	
+	public File makeMetadataFile(String fileName,String pipType) {
+		return Path.makeFile(n.getWorkAreaRootPath(),WA_PIPS,pipType,o.getContractor().getShort_name(),
+				o.getIdentifier(),fileName+FILE_EXTENSION_XML);
+	}
+	
+	public File makeFileInSrcFolder(String fileName,String pipType) {
+		return Path.makeFile(n.getWorkAreaRootPath(),WA_PIPS,pipType,o.getContractor().getShort_name(),
+				o.getIdentifier()+UNDERSCORE+o.getPackages().get(0).getName(),fileName);
+	}
+	
 	private File makePIPFolder(String pipType) {
 		return Path.makeFile(n.getWorkAreaRootPath(),WA_PIPS,pipType,o.getContractor().getShort_name(),o.getIdentifier());
+	}
+	
+	private File makePIPSourceFolder(String pipType) {
+		return Path.makeFile(n.getWorkAreaRootPath(),WA_PIPS,pipType,o.getContractor().getShort_name(),o.getIdentifier()+UNDERSCORE+o.getLatestPackage().getId());
 	}
 }
