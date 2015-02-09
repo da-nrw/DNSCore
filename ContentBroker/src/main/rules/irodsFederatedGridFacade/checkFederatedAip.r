c73cfd2359fe5aeabf50f2d32f4cbd0a101cafb2
jenspeters <peters@kcg.de>
2015-02-09 15:53:02 +0100

    refactored checkFederatedCopy.r

--- ContentBroker/src/main/rules/irodsFederatedGridFacade/checkFederatedAip.r
+++ ContentBroker/src/main/rules/irodsFederatedGridFacade/checkFederatedAip.r
@@ -17,46 +17,11 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 checkFederatedItemsOnLocalZone {
-	delay("<EF>60m</EF>") {
+#	delay("<EF>60m</EF>") {
 	acLog("---Audit Service Started---")
-	*i=1
-	msiExecStrCondQuery("SELECT DATA_NAME, COLL_NAME, META_DATA_ATTR_NAME, META_DATA_ATTR_VALUE where COLL_NAME like '%/federated/%' and META_DATA_ATTR_NAME = 'last_checked' ORDER BY META_DATA_ATTR_VALUE ASC",*checkDaos)	
-	foreach(*checkDaos) {
-		acLog("No. *i")
-		*status=0
-		msiGetValByKey(*checkDaos,"DATA_NAME",*checkDao);
-		msiGetValByKey(*checkDaos,"COLL_NAME",*checkColl);
-		msiGetValByKey(*checkDaos,"META_DATA_ATTR_VALUE",*lc);
-		*dao="*checkColl/*checkDao"
-		acLog("checking ... *dao")
-		acNeedCheck(*dao,*need,*trustYears)   
-		
-		if (*need==1) {
-			msiDataObjChksum(*dao,"forceChksum=",*localCs)
-        		acGetOrigChecksum(*dao,*origCs)
-			acVerifyChecksum(*dao,*status)	
-			if (*status==1) {
-				if (*localCs != *origCs) {
-					acLog("Federated COPY in E R R O R: *dao")
-				} else {
-					acLog("Copy seem to be OK")
-				}
-			} else {
-				acLog("Federated COPY in E R R O R: *dao")
-				if (*admin!="test@test.de"){
-					msiSendEmail(*admin,"DNS-ERROR of federated copy",*dao)				
-				}
-			}
-
-		  
-		} else {
-			acLog("No check needed on foreign copies yet!")
-		} 	
-		*i=*i+1		
-		if (*i>=5) { break }
-	}
-acLog("---Audit Service ended---")
+        acCheckRecievedFederatedCopies(*admin, *numbersPerRun,*trustYears)	
+	acLog("---Audit Service ended---")
+#}
 }
-}
-INPUT *zone=$"zone", *admin=$"test@test.de", *numbersPerRun=$5,*trustYears=$0
+INPUT *admin=$"test@test.de", *numbersPerRun=$5,*trustYears=$0
 OUTPUT ruleExecOut
