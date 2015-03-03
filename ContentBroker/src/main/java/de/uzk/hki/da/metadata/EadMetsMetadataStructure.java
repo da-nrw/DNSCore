cdf2bbae9d6f68a99a3e82667cc609dca65ec0be
Polina Gubaidullina <polinagubai@web.de>
2015-03-03 15:26:28 +0100

    implements the edm elements edm:isShownBy

--- ContentBroker/src/main/java/de/uzk/hki/da/metadata/EadMetsMetadataStructure.java
+++ ContentBroker/src/main/java/de/uzk/hki/da/metadata/EadMetsMetadataStructure.java
@@ -204,7 +204,7 @@ public class EadMetsMetadataStructure extends MetadataStructure{
 		try {
 			href = daogrp.getChild("daoloc").getAttributeValue("href");
 		} catch (Exception e) {
-//			logger.error("No unitdate element found");
+			logger.error("No unitdate element found");
 		}
 		hrefs.add(href);
 		return hrefs;
