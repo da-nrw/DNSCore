1668bda2638a6590fbd15988f7f23f7470c9804d
Polina Gubaidullina <polinagubai@web.de>
2015-08-03 10:29:38 +0200

    migration decision timeout

---
+++ ContentBroker/src/main/java/de/uzk/hki/da/core/UserDecisionTimeoutWorker.java
@@ -0,0 +1,96 @@
+package de.uzk.hki.da.core;
+
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.slf4j.MDC;
+
+import de.uzk.hki.da.model.Job;
+import de.uzk.hki.da.model.Node;
+import de.uzk.hki.da.service.HibernateUtil;
+
+/**
+ * @author Josef Hammer
+ */
+public class UserDecisionTimeoutWorker extends Worker {
+
+	private Node localNode; // Node the ContentBroker actually runs on
+
+	public void init() {
+		Session session = HibernateUtil.openSession();
+		session.refresh(localNode);
+		session.close();
+	}
+
+	@Override
+	public void scheduleTaskImplementation() {
+		try {
+			this.updateTimedOut();
+		} catch (Exception exc) {
+			logger.error("Error executing:", exc);
+		}
+	}
+
+	protected void updateTimedOut() {
+		logger.debug("updateTimedOut");
+
+		Session session = HibernateUtil.openSession();
+
+		String dateBefore = String.valueOf(new Date().getTime() / 1000L - 864000 * 30);
+
+		String queryStr = "SELECT j FROM Job j LEFT JOIN j.obj o "
+				+ "where j.status=?1 and j.date_modified < ?2 and j.responsibleNodeName=?3";
+
+		String waitUserDecStatus = C.WORKFLOW_STATUS_WAIT___PROCESS_FOR_USER_DECISION_ACTION;
+		Query query = session.createQuery(queryStr);
+		query.setParameter("1", waitUserDecStatus);
+		query.setParameter("2", dateBefore);
+		query.setParameter("3", this.localNode.getName());
+
+		@SuppressWarnings("unchecked")
+		List<Job> jobList = query.list();
+
+		if (jobList.isEmpty()){
+			return;
+		}
+
+		String newStatus = switchStatus(waitUserDecStatus, C.WORKFLOW_STATUS_DIGIT_WAITING);
+		Transaction transi = session.beginTransaction();
+
+		for (int iii = 0; iii < jobList.size(); iii++) {
+			Job jobbi = jobList.get(iii);
+			
+			jobbi.setStatus(newStatus);
+			jobbi.setAnswer(C.ANSWER_TO);
+			jobbi.setDate_modified(String.valueOf(new Date().getTime() / 1000L));
+			session.save(jobbi);
+
+			logger.debug("found identifier: " + jobbi.getObject().getIdentifier()
+					+ " origname: " + jobbi.getObject().getOrig_name() 
+					+ " newStatus: " + jobbi.getStatus());
+		}
+		transi.commit();
+		session.close();
+	}
+
+	private String switchStatus(String startStatus,String digit) {
+		return startStatus.substring(0,startStatus.length()-1) + digit;
+	}
+
+	@Override
+	public void setMDC() {
+		MDC.put(WORKER_ID, "deciciontimeout");
+	}
+
+	public Node getLocalNode() {
+		return localNode;
+	}
+
+	public void setLocalNode(Node localNode) {
+		this.localNode = localNode;
+	}
+
+}
---
+++ ContentBroker/src/test/java/de/uzk/hki/da/at/ATMigrationDecisionTimeout.java
@@ -0,0 +1,84 @@
+package de.uzk.hki.da.at;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.uzk.hki.da.core.C;
+import de.uzk.hki.da.model.Job;
+import de.uzk.hki.da.model.Object;
+import de.uzk.hki.da.service.HibernateUtil;
+import de.uzk.hki.da.util.Path;
+
+/**
+ * @author Josef Hammer
+ */
+public class ATMigrationDecisionTimeout extends AcceptanceTest {
+	private static final String ORIG_NAME_NOTALLOWED = "ATMigrationDecisionTimeout";
+	private static final File UNPACKED_TMP = new File("/tmp/DecisionUnpacked");
+
+	@Before
+	public void setUp() throws IOException {
+		ath.putSIPtoIngestArea(ORIG_NAME_NOTALLOWED, C.FILE_EXTENSION_TGZ,
+				ORIG_NAME_NOTALLOWED);
+	}
+
+	@After
+	public void tearDown() {
+		FileUtils.deleteQuietly(UNPACKED_TMP);
+	}
+
+	@Test
+	public void testMigrationTimeout() throws IOException, InterruptedException {
+
+		ath.waitForJobToBeInStatus(ORIG_NAME_NOTALLOWED,
+				C.WORKFLOW_STATUS_WAIT___PROCESS_FOR_USER_DECISION_ACTION);
+
+		Job jobbi = ath.getJob(ORIG_NAME_NOTALLOWED);
+
+		String notRealyLongAgo = String
+				.valueOf(new Date().getTime() / 1000L - 864000 * 30);
+
+		Session session = HibernateUtil.openSession();
+		session.refresh(jobbi);
+
+		Object obbi = jobbi.getObject();
+		Transaction trans = session.beginTransaction();
+		jobbi.setDate_modified(notRealyLongAgo);
+		session.save(jobbi);
+		trans.commit();
+		session.close();
+
+		ath.awaitObjectState(ORIG_NAME_NOTALLOWED,
+				Object.ObjectStatus.ArchivedAndValidAndNotInWorkflow);
+
+		ath.retrieveAIP(obbi, UNPACKED_TMP, "1");
+
+		String aDir = "", bDir = "";
+		File[] fList = Path.makeFile(UNPACKED_TMP.toString(), "data")
+				.listFiles();
+		for (File file : fList) {
+			if (file.getAbsolutePath().endsWith("+a")) {
+				aDir = FilenameUtils.getBaseName(file.getAbsolutePath());
+			} else if (file.getAbsolutePath().endsWith("+b")) {
+				bDir = FilenameUtils.getBaseName(file.getAbsolutePath());
+			}
+		}
+
+		File orgFile = Path.makeFile(UNPACKED_TMP.toString(), "data", aDir,	"v.bmp");
+		assertTrue(orgFile.exists());
+
+		File migFile = Path.makeFile(UNPACKED_TMP.toString(), "data", bDir,	"v.tif");
+		assertTrue(!migFile.exists());
+	}
+}
