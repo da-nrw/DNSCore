cea0aafe9253bde07f482fd87f8e40889119158d
jenspeters <peters@kcg.de>
2015-08-04 13:42:35 +0200

    comments added

--- ContentBroker/src/main/java/de/uzk/hki/da/grid/IrodsCopyExecutor.java
+++ ContentBroker/src/main/java/de/uzk/hki/da/grid/IrodsCopyExecutor.java
@@ -1,5 +1,29 @@
+/*
+  DA-NRW Software Suite | ContentBroker
+  Copyright (C) 2013 Historisch-Kulturwissenschaftliche Informationsverarbeitung
+  Universität zu Köln, 2014 LVR InfoKom
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package de.uzk.hki.da.grid;
 
+/**
+ * Uses iRODs irsync to copy files to secondary nodes
+ * 
+ * @author Jens Peters
+ */
 import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,7 +61,7 @@ public class IrodsCopyExecutor implements JobExecutor {
 				logger.info("Creating " + targetDir + " now");
 				iclc.mkCollection(targetDir);
 			}
-			logger.debug("RSYNC "+cj.getSource()+" & "+targetDir+" with params "+cj.getParams());
+			logger.debug("iRSYNC "+cj.getSource()+" & "+targetDir+" with params "+cj.getParams());
 			String out = iclc.rsync(cj.getSource(), targetDir, cj.getParams());
 			logger.debug(out);
 			if (out.contains("ERROR")) {
