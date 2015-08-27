acf0030961773e49204adae9d4905f223d8d2194
Polina Gubaidullina <polinagubai@web.de>
2015-08-27 18:30:26 +0200

    new test for premis reader

---
+++ ContentBroker/src/test/java/de/uzk/hki/da/model/ObjectPremisXmlReaderTests.java
@@ -0,0 +1,44 @@
+package de.uzk.hki.da.model;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+
+import org.junit.Test;
+
+import ch.qos.logback.classic.Logger;
+import de.uzk.hki.da.util.Path;
+import de.uzk.hki.da.util.RelativePath;
+
+
+
+public class ObjectPremisXmlReaderTests {
+
+	private static final Path WORK_AREA_ROOT_PATH = new RelativePath("src/test/resources/model/ObjectPremisXmlReader/");
+	private File premis = Path.makeFile(WORK_AREA_ROOT_PATH, "premis.xml");
+	
+	@Test
+	public void parsePremisTest() {
+		
+		Object o = null;
+		
+		try {
+			o = new ObjectPremisXmlReader()
+			.deserialize(premis);
+		} catch (NullPointerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if (!o.grantsRight("MIGRATION")) {
+			System.out.println("");
+		}
+	}
+	
+}
