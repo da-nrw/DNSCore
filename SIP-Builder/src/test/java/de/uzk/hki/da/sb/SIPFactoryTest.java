d308e41d871234b2967293e99a949376c1e8cda0
Polina Gubaidullina <polinagubai@web.de>
2015-09-24 16:17:18 +0200

    clean up

--- SIP-Builder/src/test/java/de/uzk/hki/da/sb/SIPFactoryTest.java
+++ SIP-Builder/src/test/java/de/uzk/hki/da/sb/SIPFactoryTest.java
@@ -246,9 +246,6 @@ public class SIPFactoryTest {
 	@Test
 	public void testBuildNestedSIPs() throws Exception {
 		
-		System.out.println("build_parameter: "+System.getenv("version"));
-		System.out.println("build_number: "+System.getenv("build_number"));
-		
 		String fixedUrn1 = "urn+nbn+de+hbz+6+1-3602";
 		String fixedUrn2 = "urn+nbn+de+hbz+42";
 		
