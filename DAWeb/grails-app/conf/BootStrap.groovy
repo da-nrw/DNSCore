ea2ca2d7ca1b048a48781ad6c2da96c555ac224e
jenspeters <peters@kcg.de>
2015-10-15 11:27:32 +0200

    reset of passwords in populatetestdb.sh for CI Environments

--- DAWeb/grails-app/conf/BootStrap.groovy
+++ DAWeb/grails-app/conf/BootStrap.groovy
@@ -7,23 +7,23 @@ class BootStrap {
     def init = { servletContext ->
 		
 		if ((Environment.current ==  Environment.DEVELOPMENT) || (Environment.current == Environment.TEST)) {	
-			println "BOOTSTRAPPED Environment activated"
-			User testuser = User.findByUsername('rods')
-			testuser.setPassword("rods")
-			if (!testuser.save(flush: true)) {
-				println "Error updating Admin DEV User!"
-				testuser.errors.allErrors.each {
-					println it
-				}
-			}
-			testuser = User.findByUsername('TEST')
-			testuser.setPassword("TEST")
-			if (!testuser.save(flush: true)) {
-				println "Error updating TEST DEV User!"
-				testuser.errors.allErrors.each {
-					println it
-				}
-			}
+			println "BOOTSTRAPPED Environment activated on CI - log in with rods/rods TEST/TEST"
+			//User testuser = User.findByUsername('rods')
+			//testuser.setPassword("rods")
+			//if (!testuser.save(flush: true)) {
+			//	println "Error updating Admin DEV User!"
+			//	testuser.errors.allErrors.each {
+			//		println it
+			//	}
+			//}
+			//testuser = User.findByUsername('TEST')
+			//testuser.setPassword("TEST")
+			//if (!testuser.save(flush: true)) {
+			//	println "Error updating TEST DEV User!"
+			//	testuser.errors.allErrors.each {
+			//		println it
+			//	}
+			//}
 
 		}
 	}
