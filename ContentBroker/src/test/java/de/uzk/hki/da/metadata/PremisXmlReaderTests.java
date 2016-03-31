32b2d0b23fec461cb2c14208126278ecbc671890
Eugen Trebunski <eugen.trebunski@gmx.de>
2016-03-31 11:41:17 +0200

    added premis validating by xsd and min-content

--- ContentBroker/src/test/java/de/uzk/hki/da/metadata/PremisXmlReaderTests.java
+++ ContentBroker/src/test/java/de/uzk/hki/da/metadata/PremisXmlReaderTests.java
@@ -15,7 +15,7 @@
 
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 package de.uzk.hki.da.metadata;
 
 import static org.junit.Assert.assertEquals;
@@ -25,18 +25,29 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.text.ParseException;
 import java.util.Calendar;
 
-import org.junit.Test;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
 
+import org.junit.Test;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import de.uzk.hki.da.core.UserException;
+import de.uzk.hki.da.core.UserException.UserExceptionId;
 import de.uzk.hki.da.model.Event;
 import de.uzk.hki.da.model.MigrationRight;
 import de.uzk.hki.da.model.Object;
 import de.uzk.hki.da.model.ObjectPremisXmlReader;
 import de.uzk.hki.da.model.Package;
+import de.uzk.hki.da.model.PremisXmlValidator;
 import de.uzk.hki.da.model.PublicationRight.Audience;
-
+import de.uzk.hki.da.utils.Path;
 
 /**
  * The Class PremisXmlReaderTests.
@@ -46,74 +57,127 @@ public class PremisXmlReaderTests {
 	/**
 	 * Test.
 	 *
-	 * @throws IOException Signals that an I/O exception has occurred.
-	 * @throws ParseException the parse exception
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 * @throws ParseException
+	 *             the parse exception
 	 */
 	@Test
-	public void test() throws IOException, ParseException{
+	public void test() throws IOException, ParseException {
 		ObjectPremisXmlReader reader = new ObjectPremisXmlReader();
-		Object premisObject = reader.deserialize(new File("src/test/resources/metadata/premisMainSection.xml"));
-		
+		Object premisObject = reader.deserialize(new File(
+				"src/test/resources/metadata/premisMainSection.xml"));
+
 		Package pkg = premisObject.getPackages().get(0);
-		
-		assertEquals("2013_07_31+11_54+a",pkg.getFiles().get(0).getRep_name());
-		assertEquals("premis.xml",pkg.getFiles().get(0).getRelative_path());
-		assertEquals("09d47bde493c1ccc5dcf0ea682c67054",pkg.getFiles().get(0).getChksum());
-		assertEquals("3844",pkg.getFiles().get(0).getSize());
-		
-		assertEquals("CONVERT",pkg.getEvents().get(0).getType());
-		assertEquals("SIP_CREATION",pkg.getEvents().get(1).getType());
-		assertEquals("INGEST",pkg.getEvents().get(2).getType());
+
+		assertEquals("2013_07_31+11_54+a", pkg.getFiles().get(0).getRep_name());
+		assertEquals("premis.xml", pkg.getFiles().get(0).getRelative_path());
+		assertEquals("09d47bde493c1ccc5dcf0ea682c67054", pkg.getFiles().get(0)
+				.getChksum());
+		assertEquals("3844", pkg.getFiles().get(0).getSize());
+
+		assertEquals("CONVERT", pkg.getEvents().get(0).getType());
+		assertEquals("SIP_CREATION", pkg.getEvents().get(1).getType());
+		assertEquals("INGEST", pkg.getEvents().get(2).getType());
 
 		assertEquals("CONTRACTOR", pkg.getEvents().get(2).getAgent_type());
 		assertEquals("TEST", pkg.getEvents().get(2).getAgent_name());
 		assertEquals("APPLICATION", pkg.getEvents().get(1).getAgent_type());
-		assertEquals("DA-NRW SIP-Builder 0.5.3", pkg.getEvents().get(1).getAgent_name());
-		
+		assertEquals("DA-NRW SIP-Builder 0.5.3", pkg.getEvents().get(1)
+				.getAgent_name());
+
 		// test events
-		for (Event e: pkg.getEvents()){
+		for (Event e : pkg.getEvents()) {
 			System.out.println(e);
 		}
-		
+
 	}
-	
-	
+
 	/**
 	 * Test rights.
 	 *
-	 * @throws IOException Signals that an I/O exception has occurred.
-	 * @throws ParseException the parse exception
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 * @throws ParseException
+	 *             the parse exception
 	 */
 	@Test
 	public void testRights() throws IOException, ParseException {
-		
+
 		ObjectPremisXmlReader reader = new ObjectPremisXmlReader();
-		Object premisObject = reader.deserialize(new File("src/test/resources/metadata/premis_xml_metadata_reader_test.xml"));
-		
+		Object premisObject = reader
+				.deserialize(new File(
+						"src/test/resources/metadata/premis_xml_metadata_reader_test.xml"));
+
 		System.out.println(premisObject.getRights());
 		assertNotNull(premisObject.getRights());
 		assertNotNull(premisObject.getRights().getPublicationRights());
 		assertFalse(premisObject.getRights().getPublicationRights().isEmpty());
-		assertEquals("640",premisObject.getRights().getPublicationRights().get(0).getImageRestriction().getWidth());
-		assertEquals("480",premisObject.getRights().getPublicationRights().get(0).getImageRestriction().getHeight());
-		
+		assertEquals("640", premisObject.getRights().getPublicationRights()
+				.get(0).getImageRestriction().getWidth());
+		assertEquals("480", premisObject.getRights().getPublicationRights()
+				.get(0).getImageRestriction().getHeight());
+
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTimeInMillis(0);
 		calendar.set(2023, 5, 23, 0, 0, 0);
-		
-		assertEquals(MigrationRight.Condition.NOTIFY,premisObject.getRights().getMigrationRight().getCondition());
-		
-		assertEquals("ePflicht",premisObject.getRights().getPublicationRights().get(0).getLawID());
-		assertEquals(2,premisObject.getRights().getPublicationRights().get(0).getTextRestriction().getCertainPages().length);
-		assertEquals(23,premisObject.getRights().getPublicationRights().get(0).getTextRestriction().getCertainPages()[0]);
-		assertEquals(42,premisObject.getRights().getPublicationRights().get(0).getTextRestriction().getCertainPages()[1]);
-		
+
+		assertEquals(MigrationRight.Condition.NOTIFY, premisObject.getRights()
+				.getMigrationRight().getCondition());
+
+		assertEquals("ePflicht", premisObject.getRights()
+				.getPublicationRights().get(0).getLawID());
+		assertEquals(2, premisObject.getRights().getPublicationRights().get(0)
+				.getTextRestriction().getCertainPages().length);
+		assertEquals(23, premisObject.getRights().getPublicationRights().get(0)
+				.getTextRestriction().getCertainPages()[0]);
+		assertEquals(42, premisObject.getRights().getPublicationRights().get(0)
+				.getTextRestriction().getCertainPages()[1]);
+
 		assertTrue(!premisObject.grantsPublicationRight(Audience.PUBLIC));
 		assertTrue(!premisObject.grantsPublicationRight(Audience.INSTITUTION));
-		
+
+	}
+
+	@Test
+	public void testXSDValidation() throws IOException, ParseException, SAXException {
+		assertTrue(PremisXmlValidator.validatePremisFile(new File(
+				"src/test/resources/metadata/sipbuilder201603_premis.xml")));
 	}
 	
+	@Test(expected=SAXParseException.class)
+	public void testXSDValidationFailWrongAttributeType() throws IOException, ParseException, SAXException {
+		assertFalse(PremisXmlValidator.validatePremisFile(new File(
+				"src/test/resources/metadata/sipbuilder201603_premisWrongTextRestrictionValue.xml")));
+	}
 	
+	@Test(expected=SAXParseException.class)
+	public void testXSDValidationFailAdditionlAttribute() throws IOException, ParseException, SAXException {
+		assertFalse(PremisXmlValidator.validatePremisFile(new File(
+				"src/test/resources/metadata/sipbuilder201603_premisFailRestrictAudioAdditionalAttribute.xml")));
+	}
 	
+	//TODO:test without mincontent -> fails 
 	
+
+	@Test
+	public void testXSDWebAvailability() throws IOException, ParseException {
+		String[] urlStrings = new String[] {
+				"http://www.loc.gov/standards/premis/v2/premis-v2-2.xsd",
+				"http://www.danrw.de/schemas/contract/v1/danrw-contract-1.xsd",
+				"http://www.loc.gov/standards/xlink/xlink.xsd" };
+		
+		for (String urlString : urlStrings) {
+			Source sourceFromXSD=null;
+			try {
+				URLConnection conn = new URL(urlString).openConnection();
+				conn.setConnectTimeout(5000);
+				conn.setReadTimeout(5000);
+				sourceFromXSD = new StreamSource(conn.getInputStream());
+			} catch (SocketTimeoutException e) {
+				assertNotNull(urlString + " XSD unavailable", sourceFromXSD);
+			}
+		}
+	}
+
 }
--- DNSCommon/src/main/java/de/uzk/hki/da/utils/C.java
+++ DNSCommon/src/main/java/de/uzk/hki/da/utils/C.java
@@ -92,6 +92,7 @@ public class C {
 	// File system
 	public static final Path CONF = new RelativePath("conf");
 	public static final String PREMIS_XSD_PATH = "conf/premis.xsd";
+	public static final String CONTRACT_XSD_PATH = "conf/danrw-contract-1.xsd";
 	public static final String CONFIG_PROPS = "conf/config.properties";
 	public static final File HIBERNATE_CFG = new RelativePath(CONF,"hibernateCentralDB.cfg.xml").toFile();
 	public static final File XLINK_XSD = new File("src/main/xsd/xlink.xsd");
@@ -184,6 +185,8 @@ public class C {
 	public static final String KIND_OF_SIPBUILDING_SINGLE_WITH_TARGET_FOLDER_NAME="Einzelnes SIP mit dem angegebenen Namen aus dem Quellordner erstellen";
 	public static final String KIND_OF_SIPBUILDING_MULTIPLE="SIPs aus Unterordnern des Quellverzeichnisses erstellen";
 	public static final String KIND_OF_SIPBUILDING_NESTED="SIPs aus verzweigter Ordnerstruktur erstellen (nur METS)";
+
+
 	
 }
 
