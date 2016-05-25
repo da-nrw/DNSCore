47fa5d7d7411c79ad7cb4518c472fc83d96e4739
Eugen Trebunski <eugen.trebunski@gmx.de>
2016-05-25 07:33:57 +0200

    Rename JhoveMetadataExtractor back to avoid configuration modification

--- ContentBroker/src/main/java/de/uzk/hki/da/format/JhoveMetadataExtractor.java
+++ ContentBroker/src/main/java/de/uzk/hki/da/format/JhoveMetadataExtractor.java
@@ -45,9 +45,9 @@ import de.uzk.hki.da.utils.StringUtilities;
  * @author Daniel M. de Oliveira
  *
  */
-public class JhoveMetadataExtractorAndVerifier implements MetadataExtractor {
+public class JhoveMetadataExtractor implements MetadataExtractor {
 
-	private static final Logger logger = LoggerFactory.getLogger(JhoveMetadataExtractorAndVerifier.class);
+	private static final Logger logger = LoggerFactory.getLogger(JhoveMetadataExtractor.class);
 	private List<JHoveParameterMapping> possibleOptions=null;
 	private List<FormatMapping> pronomMimetypeList=null;
 	
@@ -72,63 +72,70 @@ public class JhoveMetadataExtractorAndVerifier implements MetadataExtractor {
 	private boolean connectabilityProved=false;
 	
 	
-	
 	/**
-	 * Scans a file with jhove and extracts technical metadata to a xml file. 
+	 * Scans a file with jhove and extracts technical metadata to a xml file.
 	 * Tries it a second time if the first time fails.
 	 * 
-	 * @throws ConnectionException when timeout limit reached two times. 
-	 * @throws IOException 
+	 * @throws ConnectionException
+	 *             when timeout limit reached two times.
+	 * @throws IOException
 	 */
-	public void extract(File file, File extractedMetadata, String expectedPUID) throws ConnectionException, IOException {
-		
-		if (!connectabilityProved) throw new IllegalStateException("Make sure you run isExecutable first.");
-		
-		if (cli==null) throw new IllegalStateException("cli not set");
-		if (!file.exists()) 
-			throw new FileNotFoundException("Missing file or directory: "+file);
+	public void extract(File file, File extractedMetadata, String expectedPUID)
+			throws ConnectionException, IOException {
+
+		if (!connectabilityProved)
+			throw new IllegalStateException("Make sure you run isExecutable first.");
+
+		if (cli == null)
+			throw new IllegalStateException("cli not set");
+		if (!file.exists())
+			throw new FileNotFoundException("Missing file or directory: " + file);
 		if (!extractedMetadata.getParentFile().exists())
-			throw new IllegalArgumentException("ParentFolder "+extractedMetadata.getParentFile()+" must exist in order to create "+extractedMetadata);
-		
+			throw new IllegalArgumentException("ParentFolder " + extractedMetadata.getParentFile() + " must exist in order to create " + extractedMetadata);
 
-		int retval=0;
+		int retval = 0;
 		getJHoveOptionForMimeType("");
-		String typeOptions=getJHoveOptionForMimeType(getMimeTypeForPronom(expectedPUID));
+		String typeOptions = getJHoveOptionForMimeType(getMimeTypeForPronom(expectedPUID));
 		try {
-			retval=execCMD(jhoveCmd(extractedMetadata, makeFilePath(file),typeOptions));
-		}catch(IOTimeoutException timeout){
+			retval = execCMD(jhoveCmd(extractedMetadata, makeFilePath(file), typeOptions));
+		} catch (IOTimeoutException timeout) {
 			logger.warn(timeout.getMessage());
-			retval=1;
-		}catch(IOException e) {
+			retval = 1;
+		} catch (IOException e) {
 			logger.warn(e.getMessage());
-			retval=1;
-		} 
-		if (retval==0) return;
-		
-		logger.info("Problem during extracting technical metadata. Will retry without parsing the whole file.");
-		
-		retval=0;
-		try {
-			retval=execCMD(jhoveCmdSkipWholeFileParsing(extractedMetadata, makeFilePath(file),typeOptions));
-		}catch(IOTimeoutException timeout) {
-			throw new ConnectionException("Second call to JHOVE ended with possible timeout (the 2nd time already).",timeout);
-		}catch(IOException e) {
-			throw new IOException("Second call to JHOVE ended with IOError (the 2nd time already).",e);
+			retval = 1;
 		}
+		if (retval != 0) {
+			// throw new
+			// ConnectionException("Recieved not null return value from jhove.");
 
-		if (retval!=0)
+			logger.info("Problem during extracting technical metadata. Will retry without parsing the whole file.");
+
+			retval = 0;
+			try {
+				retval = execCMD(jhoveCmdSkipWholeFileParsing(extractedMetadata , makeFilePath(file), typeOptions));
+			} catch (IOTimeoutException timeout) {
+				throw new ConnectionException("Second call to JHOVE ended with possible timeout (the 2nd time already).", timeout);
+			} catch (IOException e) {
+				throw new IOException("Second call to JHOVE ended with IOError (the 2nd time already).", e);
+			}
+		}
+		if (retval != 0)
 			throw new ConnectionException("Recieved not null return value from jhove.");
-		
+
 		JhoveResult jhResult;
 		try {
 			jhResult = JhoveResult.parseJHoveXML(extractedMetadata.getAbsolutePath());
 		} catch (SAXException e) {
-			//e.printStackTrace();
-			throw new RuntimeException("JHove Output("+extractedMetadata.getAbsolutePath()+") not interpretable: "+e.getMessage(),e);
+			// e.printStackTrace();
+			logger.error("JHove Output(" + extractedMetadata.getAbsolutePath() + ") not interpretable: " + e.getMessage());
+			throw new JHoveValidationException("JHove Output(" + extractedMetadata.getAbsolutePath() + ") not interpretable: " + e.getMessage(), e);
 		}
-		
-		if(!jhResult.isValid())
-			throw new RuntimeException("JHove say "+file+" is not valid: "+jhResult);
+
+		if (!jhResult.isValid()){
+			logger.error("JHove say " + file + " is not valid: " + jhResult);
+			throw new JHoveValidationException("JHove say " + file + " is not valid: " + jhResult);
+		}
 	}
 	
 	
@@ -239,8 +246,11 @@ public class JhoveMetadataExtractorAndVerifier implements MetadataExtractor {
 		FormatMapping searchFor=new FormatMapping();
 		searchFor.setPuid(puid);
 		int indexOfSearchFor=Collections.binarySearch(getPronomTable(), searchFor,  formatMappingComparator);
-		if(indexOfSearchFor<0)
-			throw new RuntimeException("Given pronom id("+puid+") is not in database");
+		if(indexOfSearchFor<0){
+			logger.debug("Given pronom id("+puid+") is not in database");
+			//throw new RuntimeException("Given pronom id("+puid+") is not in database");
+			return "";
+		}
 		return getPronomTable().get(indexOfSearchFor).getMime_type();
 	}
 	
@@ -253,19 +263,11 @@ public class JhoveMetadataExtractorAndVerifier implements MetadataExtractor {
 		session.beginTransaction();
 		//List<Job> l = null;
 	
-		pronomMimetypeList = session.createQuery(
-					//"SELECT j FROM FormatMapping j left join j.obj as o left join o.user as c where o.orig_name=?1 and c.short_name=?2"
-				"FROM FormatMapping"
-					)
-							//.setParameter("1", orig_name)
-							//.setParameter("2", csn)
+		pronomMimetypeList = session.createQuery("FROM FormatMapping")
 							.setReadOnly(true).list();
 		session.close();
 
-		Collections.sort(pronomMimetypeList, new Comparator<FormatMapping>(){
-			@Override public int compare(FormatMapping o1, FormatMapping o2) {
-				return o1.getPuid().compareTo(o2.getPuid());
-			}});
+		Collections.sort(pronomMimetypeList, formatMappingComparator);
 		}
 		return pronomMimetypeList;
 	}
