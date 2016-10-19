707054b98d2ada44d062cdf305a40a1df0d30b0e
Eugen Trebunski <eugen.trebunski@gmx.de>
2016-10-19 12:31:11 +0200

    Graceful shutdown to set jobs in secure state before kill

--- ContentBroker/src/main/bash/ContentBroker_stop.sh
+++ ContentBroker/src/main/bash/ContentBroker_stop.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-kill -9 `ps -aef | grep ContentBroker.jar | grep -v grep | awk '{print $2}'`
+MAX_TIME=30 
+STEP=5
 
+PID=`ps -aef | grep ContentBroker.jar | grep -v grep | awk '{print $2}'`
+
+if [ "$PID" == "" ]; then
+	echo "Application is not running"
+else
+	kill -15 $PID  # for graceful shutdown
+	sleep 1
+	CUR_TIME=0
+	while [[ $CUR_TIME -lt $MAX_TIME ]]
+	do
+		CUR_TIME=$((CUR_TIME +STEP))
+		if [  `ps --pid $PID | wc -l` -eq 1 ]; then
+			break;
+		fi
+		echo "Wait for gracefully exiting of ContentBroker $CUR_TIME/$MAX_TIME s"
+		sleep $STEP
+	done
+	
+	if [  `ps --pid $PID | wc -l` -eq 2 ]; then
+		kill -9 $PID
+	fi
+fi
 rm -f /tmp/cb.running
