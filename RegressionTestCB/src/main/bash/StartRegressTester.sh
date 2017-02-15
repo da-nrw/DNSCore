66986e171715c1b84ccfd13b7864aeb4129a9e3b
trebunski <eugen.trebunski@gmx.de>
2017-02-15 09:41:04 +0100

    Update StartRegressTester.sh

--- RegressionTestCB/src/main/bash/StartRegressTester.sh
+++ RegressionTestCB/src/main/bash/StartRegressTester.sh
@@ -3,7 +3,7 @@
 #echo "Prepare Index"
 #conf/rebuildTestIndex.sh
 
-java -jar ./RegressionTestCBFull.jar $@ |  tee -a regressExec`date "+%Y%m%d-%H%M%S"`.txt
+java -jar ./RegressionTestCBFull.jar $@ |  tee -a regressLog`date "+%Y%m%d-%H%M%S"`.txt
 
 #echo "Cleanup Index"
 #conf/rebuildTestIndex.sh
