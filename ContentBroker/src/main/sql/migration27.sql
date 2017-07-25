ec5bd429b176428d766df929e907dd8498f63e79
Z1334005 <eugen.trebunski@gmx.de>
2017-07-25 21:41:52 +0200

    migration27.sql sollte ausgeführt werden

---
+++ ContentBroker/src/main/sql/migration27.sql
@@ -0,0 +1,9 @@
+begin;
+
+ALTER TABLE objects DROP COLUMN IF EXISTS quality_flag;
+ALTER TABLE objects ADD COLUMN quality_flag INTEGER;
+
+/*old objects have quality level 3 becouse there were already migrateable in past*/
+update objects SET quality_flag=3 WHERE quality_flag IS NULL;
+
+commit;
