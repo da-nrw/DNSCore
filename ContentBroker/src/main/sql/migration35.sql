539a867dba880772a6b9b79329f05ec447d745c1
trebunski <eugen.trebunski@lvr.de>
2019-10-10 12:05:14 +0000

    migration35.sql und migration36.sql muessen ausgefuehrt werden

--- ContentBroker/src/main/sql/migration35.sql
+++ ContentBroker/src/main/sql/migration35.sql
@@ -9,4 +9,14 @@ DELETE FROM conversion_policies WHERE source_format LIKE 'x-fmt/398' AND present
 DELETE FROM conversion_policies WHERE source_format LIKE 'x-fmt/390' AND presentation ='f';
 DELETE FROM conversion_policies WHERE source_format LIKE 'x-fmt/391' AND presentation ='f';
 
+
+/*Einrichten neuer Presentation-Conversion-Policies, damit JPG in den PIP weitergereicht werden. fmt/43 ist schon vorhanden*/
+INSERT INTO conversion_policies(id,source_format,conversion_routine_id,presentation,psystem_id) VALUES( 61,'fmt/41',8,'t',1 );
+INSERT INTO conversion_policies(id,source_format,conversion_routine_id,presentation,psystem_id) VALUES( 62,'fmt/42',8,'t',1 );
+INSERT INTO conversion_policies(id,source_format,conversion_routine_id,presentation,psystem_id) VALUES( 63,'fmt/44',8,'t',1 );
+INSERT INTO conversion_policies(id,source_format,conversion_routine_id,presentation,psystem_id) VALUES( 64,'x-fmt/390',8,'t',1 );
+INSERT INTO conversion_policies(id,source_format,conversion_routine_id,presentation,psystem_id) VALUES( 65,'x-fmt/391',8,'t',1 );
+INSERT INTO conversion_policies(id,source_format,conversion_routine_id,presentation,psystem_id) VALUES( 66,'x-fmt/398',8,'t',1 );
+
+
 commit;
